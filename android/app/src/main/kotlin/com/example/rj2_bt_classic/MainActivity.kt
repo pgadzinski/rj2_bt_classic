66b177ba44c1376ee1dd10eaf01a735c1a83c9ee
Shuebur <mdshuebur@gmail.com>
2026-05-06 10:29:07 +0600

    github upload

---
+++ android/app/src/main/kotlin/com/example/rj2_bt_classic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rj2_bt_classic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_bluetooth_classic_serial
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftFlutterBluetoothClassicPlugin.register(with: registry.registrar(forPlugin: "SwiftFlutterBluetoothClassicPlugin"))
+}
