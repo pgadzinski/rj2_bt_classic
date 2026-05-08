8e1476a4c3005a2c91b61cfe973556651f58e523
sakib556 <kazisakib556@gmail.com>
2026-05-08 23:33:22 +0600

    Add detailed logging for BLE client connections and command processing in firmare.ino. Introduced logTsPrefix and logLine functions for consistent logging, and implemented ServerCallbacks to manage BLE client connection events.

--- lib/firmare.ino
+++ lib/firmare.ino
@@ -16,6 +16,36 @@ Servo servo2;
 
 BLECharacteristic *pCharacteristic;
 
+static bool g_hasClient = false;
+
+static void logTsPrefix() {
+  Serial.print('[');
+  Serial.print(millis());
+  Serial.print(" ms] ");
+}
+
+static void logLine(const String &msg) {
+  logTsPrefix();
+  Serial.println(msg);
+}
+
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) override {
+    g_hasClient = true;
+    logLine("BLE client CONNECTED");
+  }
+
+  void onDisconnect(BLEServer *pServer) override {
+    g_hasClient = false;
+    logLine("BLE client DISCONNECTED");
+
+    // Some phones require re-advertising after disconnect.
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->start();
+    logLine("Advertising restarted");
+  }
+};
+
 class MyCallbacks : public BLECharacteristicCallbacks {
 
   void onWrite(BLECharacteristic *pCharacteristic) {
@@ -26,8 +56,7 @@ if (cmd.length() > 0) {
 
   cmd.trim();
 
-  Serial.print("Received CMD: ");
-  Serial.println(cmd);
+  logLine("RX CMD: \"" + cmd + "\" (len=" + String(cmd.length()) + ")");
 
   processCommand(cmd);
 }
@@ -43,7 +72,8 @@ if (cmd.length() > 0) {
 
       String response = "S1 -> " + String(angle) + " deg";
 
-      Serial.println(response);
+      logLine(response);
+
 
     }
     else if (cmd.startsWith("S2:")) {
@@ -54,12 +84,12 @@ if (cmd.length() > 0) {
 
       String response = "S2 -> " + String(angle) + " deg";
 
-      Serial.println(response);
+      logLine(response);
+
     }
     else {
 
-      Serial.print("Unknown Command: ");
-      Serial.println(cmd);
+      logLine("Unknown CMD: \"" + cmd + "\"");
     }
   }
 };
@@ -67,24 +97,32 @@ if (cmd.length() > 0) {
 void setup() {
 
   Serial.begin(115200);
+  delay(200);
+  logLine("Boot");
 
   servo1.attach(SERVO1_PIN);
   servo2.attach(SERVO2_PIN);
 
   servo1.write(0);
   servo2.write(0);
+  logLine("Servos attached. S1 pin=" + String(SERVO1_PIN) + ", S2 pin=" + String(SERVO2_PIN));
 
   BLEDevice::init("RJ2_BLE");
+  logLine("BLE init. Name=RJ2_BLE");
 
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+  logLine("BLE server created");
 
   BLEService *pService = pServer->createService(SERVICE_UUID);
+  logLine("Service created UUID=" + String(SERVICE_UUID));
 
   pCharacteristic = pService->createCharacteristic(
                       CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_WRITE |
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
+  logLine("Characteristic created UUID=" + String(CHARACTERISTIC_UUID));
 
   pCharacteristic->setCallbacks(new MyCallbacks());
 
@@ -96,7 +134,8 @@ void setup() {
 
   pAdvertising->start();
 
-  Serial.println("ESP32 BLE Ready");
+  logLine("Advertising started");
+  logLine("ESP32 BLE Ready");
 }
 
 void loop() {
